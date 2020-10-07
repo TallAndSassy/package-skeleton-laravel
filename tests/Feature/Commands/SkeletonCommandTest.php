9143994631dcfbcf1bd34f099b92482b68639304
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-07 17:58:34 -0400

    fix command

--- tests/Feature/Commands/SkeletonCommandTest.php
+++ tests/Feature/Commands/SkeletonCommandTest.php
@@ -8,7 +8,7 @@ class SkeletonCommandTest extends \Spatie\Skeleton\Tests\TestCase
     /** @test */
     public function test_command_works()
     {
-        $this->artisan('hw')->assertExitCode(0);
-        $this->artisan('hw')->expectsOutput('Spatie/Skeleton/hw/tbd');
+        $this->artisan('bladeprefix:somecommand')->assertExitCode(0);
+        $this->artisan('bladeprefix:somecommand')->expectsOutput('Spatie/Skeleton/hw/tbd');
     }
 }
