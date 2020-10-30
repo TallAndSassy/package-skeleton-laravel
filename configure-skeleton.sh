3674c95acc9db8548024809249a41c5825cf2d43
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-30 14:06:17 -0400

    standard routes

--- configure-skeleton.sh
+++ configure-skeleton.sh
@@ -37,7 +37,7 @@ package_description=$(ask_question "Package description" "")
 
 
 bladeprefix=$(ask_question "Blade Prefix - all lowercase and just one word, probably." "tassyvendornamelowercasepackagenamelowercase")
-languageprefix=$(ask_question "Language Prefix - all lowercase and just one word, probably." "tassyvendornamelowercasepackagenamelowercase")
+languageprefix=$(ask_question "Language Prefix - all lowercase and just one word, something globally unique." "packagename")
 
 class_name=$(echo "$package_name" | sed 's/[-_]/ /g' | awk '{for(j=1;j<=NF;j++){ $j=toupper(substr($j,1,1)) substr($j,2) }}1' | sed 's/[[:space:]]//g')
 
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+# Include this from /routes/web.php (because we're still learning how to auto-register our routes)
+
+# Route::get('/admin', fn () => redirect('/admin/dashboard'));
+
+//Route::middleware(['auth:sanctum', 'verified'])
+//    ->get(
+//        '/admin/help/{sublevels?}',
+//        [\TallAndSassy\AppThemeBaseAdmin\Http\Controllers\Admin\HelpController::class, 'getFrontView']
+//    )
+//    ->where('sublevels', '.*');
+
